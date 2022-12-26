0f01d7fd91a3c4a58f228b504cec53165917e678
EchoGronkowski <robbysmainemail@gmail.com>
2022-12-26 15:26:23 -0500

    FINALLY

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 const main = require('../lib/index')
+const https = require('node:https')
+const http = require('node:http')
 
 main.getBandJSON()
+  .then((data) => {
+    console.log(data);  // Output: JSON object from API call
+  })
+  .catch((error) => {
+    console.error(error);
+});
