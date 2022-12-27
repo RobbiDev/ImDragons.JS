7fc0d8271b1d5f217e5e799064d9afd8aac458a4
EchoGronkowski <robbysmainemail@gmail.com>
2022-12-27 12:24:59 -0500

    1.0.0

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const imaginejs = require('../lib/index')
 imaginejs.indexer.getBandJSON()
   .then((data) => {
     // Do something with the data here
-    let band = data.details
+    let band = data.details.labels[1]
     console.log(band)
   })
