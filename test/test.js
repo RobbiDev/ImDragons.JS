2e4312bd94928ea3364448024baa1325eda69371
RobbiDev <robbysmainemail@gmail.com>
2023-01-08 20:26:36 -0500

    pre-1.0.1

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 import { Collection } from "../lib/index.js"
 
 // Create a New Indexer of Band Data
-const indexer = new Collection('info')
+const indexer = await Collection('album')
 
 // Shows all Band JSON Infomation 
-console.log(indexer.details)
+console.log(indexer)
